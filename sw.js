c72b7ce117aa082a5fa147d8ce08bb0281286e8e
alda-creator <76640956+alda-creator@users.noreply.github.com>
2026-08-13 22:46:48 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cancionero-v8';
+const CACHE_NAME = 'cancionero-v9';
 
 // Lista completa de archivos de la app + dependencias externas (Firebase y fuentes)
 const urlsToCache = [
